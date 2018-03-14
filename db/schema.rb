b9cb316b366c8dd56b1897655a08c38fe4a12bee
KT <truongmkevin@yahoo.com>
2018-03-14 19:18:06 +0000

    added paperclip for image upload and edit views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314142444) do
+ActiveRecord::Schema.define(version: 20180314155323) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20180314142444) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
