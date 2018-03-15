88d82c0773531bd9c0d756a9fd938f467e1f9c6a
KT <truongmkevin@yahoo.com>
2018-03-15 14:11:59 +0000

    created storefront and added modal functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  root 'storefront#all_items'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
   resources :orders
   resources :line_items
   resources :categories
   resources :products
-
-  root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
