a47db71af5eaaaedc8e42b459bbe3d529fa24e0c
KT <truongmkevin@yahoo.com>
2018-03-19 15:13:04 +0000

    added checkout functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+categories = Category.find_or_create_by (
+  [
+    {name: "Computers"},
+    {name: "Smart Phones"},
+    {name: "Televisions"},
+    {name: "Game Consoles"},
+    {name: "Video Games"},
+    {name: "Appliances"},
+    {name: "Other"}
+  ]
+)
+
+video_games_id = Category.find_by(name: "Video Games").id
+Product.create(name: "Cuphead", price: 20.00, category_id: video_games_id)
